f8ae8f6032be2cc09e6e2162fe17c056d0f0320f
xiu <gaoxiu333@gmail.com>
2024-12-13 23:08:27 +0800

    feat: vit & ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import viteTsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
+  optimizeDeps: { exclude: ["fsevents"] },
   // 别名
   resolve: {
     alias: {
