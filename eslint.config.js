05b11714ed4a5cedabcebdfcc3c5ab404585245c
xiu <gaoxiu333@gmail.com>
2024-12-16 18:19:48 +0800

    feat: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,37 @@
+import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
-import globals from 'globals';
+import prettierConfig from 'eslint-config-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import prettierConfig from 'eslint-config-prettier';
 import tailwindcssPlugin from 'eslint-plugin-tailwindcss';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import react from '@eslint-react/eslint-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default tseslint.config(
+  // 忽略 .gitignore 中的文件
+  includeIgnoreFile(gitignorePath),
+
+  // 其他的一些插件
   ...tailwindcssPlugin.configs['flat/recommended'],
-  { ignores: ['dist', 'public/mockServiceWorker.js'] },
+
+  // 通用
+  js.configs.recommended,
+  tseslint.configs.recommended,
+
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['./src/**/*.{ts,tsx}'],
+    ...react.configs.recommended,
+  },
+  {
+    files: ['./src/**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 'latest',
       globals: globals.browser,
@@ -19,15 +39,17 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      unusedImports,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+    },
+  },
+  // 通用规则
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'react-refresh/only-export-components': 'off',
     },
   },
   prettierConfig,
