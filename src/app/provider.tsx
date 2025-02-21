7a0e10d3f31d7e02b3489cd78f37e58a2f073f3e
xiu <gaoxiu333@gmail.com>
2025-02-21 11:45:39 +0800

    init shadcn

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,6 +1,12 @@
+import { ThemeProvider } from "@/components/theme-provider";
 type AppProviderProps = {
   children: React.ReactNode;
 };
+
 export const AppProvider = ({ children }: AppProviderProps) => {
-  return children;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {children}
+    </ThemeProvider>
+  );
 };
--- src/app/routes/app-root/root.tsx
+++ src/app/routes/app-root/root.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export const AppRoot = () => {
@@ -5,6 +6,11 @@ export const AppRoot = () => {
     <div className="text-center">
       <h2 className=" text-2xl text-amber-950">我在首页的你</h2>
       <Button className="">走～ 去登录</Button>
+      <h2>看看主题</h2>
+      <div className="bg-background text-foreground">
+        <p className=" bg-primary text-primary-foreground">12323213</p>
+        <ModeToggle />
+      </div>
     </div>
   );
 };
