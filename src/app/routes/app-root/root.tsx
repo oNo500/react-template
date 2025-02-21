11bed0fa7bead3e654ef20a375f2fac33e59d720
xiu <gaoxiu333@gmail.com>
2025-02-21 10:40:54 +0800

    ui: shadcn

--- src/app/routes/app-root/root.tsx
+++ src/app/routes/app-root/root.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export const AppRoot = () => {
-  return <div className=" text-red-500 text-center">首页</div>;
+  return (
+    <div className="text-center">
+      <h2 className=" text-2xl text-amber-950">我在首页的你</h2>
+      <Button className="">走～ 去登录</Button>
+    </div>
+  );
 };
