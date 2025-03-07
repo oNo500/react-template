33459fc58e9712fc24f84e5aec72a569637dcc81
xiu <gaoxiu333@gmail.com>
2025-03-07 08:23:58 +0800

    init next

--- apps/nextjs-page/src/pages/index.tsx
+++ apps/nextjs-page/src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
+              src/pages/index.tsx
             </code>
             .
           </li>
