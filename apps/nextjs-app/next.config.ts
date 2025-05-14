02aecbe1dbaf8d79a585cebcdf19cb4196e17efd
xiu <gaoxiu333@gmail.com>
2025-05-14 23:58:49 +0800

    feat: 更新Next.js项目配置，添加.gitignore文件，优化eslint配置，删除package-lock.json

--- apps/nextjs-app/next.config.ts
+++ apps/nextjs-app/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    typedRoutes: true,
+  },
+  // TODO 在生产环境忽略类型错误
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
