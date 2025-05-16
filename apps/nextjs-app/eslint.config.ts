fa28655a23021ec1b4f3b42952dbec394d04c9c9
xiu <gaoxiu333@gmail.com>
2025-05-16 08:50:43 +0800

    feat: 更新Next.js和UI项目配置，添加增量编译支持，优化ESLint和TypeScript配置

---
+++ apps/nextjs-app/eslint.config.ts
@@ -0,0 +1,3 @@
+import { config as nextJsConfig } from '@repo/eslint-config/next-js';
+
+export default nextJsConfig;
--- packages/ui/eslint.config.ts
+++ packages/ui/eslint.config.ts
@@ -1,3 +1,11 @@
-import { config } from '@repo/eslint-config/react-internal';
+import { config as reactInternalConfig } from '@repo/eslint-config/react-internal';
 
+const config = [
+  ...reactInternalConfig,
+  {
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off', // 由于本包不是 Next.js 应用，没有 pages 目录，需关闭该规则以避免报错
+    },
+  },
+];
 export default config;
