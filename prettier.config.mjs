25a08edbb4fa5a3ced470b48158095f1f387298f
xiu <gaoxiu333@gmail.com>
2025-05-17 10:30:46 +0800

    feat: 更新项目依赖，替换eslint和typescript配置，添加tailwindcss插件，优化代码格式

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,10 +3,16 @@ const config = {
   trailingComma: 'all',
   singleQuote: true,
   printWidth: undefined,
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
   importOrder: ['^react', '^next', '^@/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
+  // tailwind 相关配置
+  tailwindFunctions: ['clsx', 'cn', 'twMerge'],
+  // tailwindAttributes: ['className', 'class'],
 };
 
 export default config;
