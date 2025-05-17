8bc1e47d7dce07a80aaaa82938085c892874ece6
xiu <gaoxiu333@gmail.com>
2025-05-17 20:48:50 +0800

    feat: 添加下拉菜单组件，更新相关依赖，优化Navbar和ModeToggle组件样式

--- apps/nextjs-app/src/components/mode-toggle.tsx
+++ apps/nextjs-app/src/components/mode-toggle.tsx
@@ -17,9 +17,13 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
+          className="inline-flex items-center justify-center p-1"
+          variant="ghost"
+          size="lg"
+        >
+          <Sun className="h-5! w-5! rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="h-5! w-5! absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
