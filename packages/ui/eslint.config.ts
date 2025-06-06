f805b7d37166b20a213b3602b5f11e47580fedf0
xiu <gaoxiu333@gmail.com>
2025-06-06 09:30:19 +0800

    feat: init storybook

--- packages/ui/eslint.config.ts
+++ packages/ui/eslint.config.ts
@@ -1,3 +1,5 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
 import { config as reactInternalConfig } from '@kit101/eslint-config/react-internal';
 
 const config = [
@@ -7,5 +9,6 @@ const config = [
       '@next/next/no-html-link-for-pages': 'off', // 由于本包不是 Next.js 应用，没有 pages 目录，需关闭该规则以避免报错
     },
   },
+  ...storybook.configs['flat/recommended'],
 ];
 export default config;
